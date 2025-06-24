15190667225650af3cdc738823916d2978047a16
Annie Rong <annierong302@gmail.com>
2025-06-23 20:00:26 -0400

    feat: implement Circle class

---
+++ Circle.java
@@ -0,0 +1,39 @@
+package lab5;
+
+public class Circle {
+	Point O; 
+	double radius; 
+	
+	public Circle(Point O, double radius) {
+		this.O = O; 
+		this.radius = radius; 
+	}
+	
+	public double circumference(double radius) {
+		return 2 * Math.PI * radius; 
+	}
+	
+	public double area(double radius) {
+		return Math.PI * Math.pow(radius, 2); 
+	}
+	
+	@Override
+	public int hashCode() {
+		return (int)(radius);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Circle other = (Circle) obj;
+		if (radius != other.radius)
+			return false;
+		return true;
+	}
+
+}
